99a2288ab50621feacc7775d6e0080b78d1d51e9
V Sonar <tejas.v.sonar@accenture.com>
2025-10-01 11:42:20 +0530

    Updated Overwrite the json file generation

--- graphql-schema-diff-poc-full/src/writeOutputs.ts
+++ graphql-schema-diff-poc-full/src/writeOutputs.ts
@@ -5,7 +5,7 @@ export async function writeReport(reportText: string, reportJson: any, outFolder
   await fs.mkdir(outFolder, { recursive: true });
   const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
   //const mdFile = path.join(outFolder, `schema-diff-report-${timestamp}.md`);
-  const jsonFile = path.join(outFolder, `schema-diff-${timestamp}.json`);
+  const jsonFile = path.join(outFolder, `schema-diff.json`);
   //await fs.writeFile(mdFile, reportText, 'utf8');
   await fs.writeFile(jsonFile, JSON.stringify(reportJson, null, 2), 'utf8');
   return { jsonFile };
