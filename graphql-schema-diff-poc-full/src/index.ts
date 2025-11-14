de850c88837932824f8550bd63a0f5f57e8fc927
V Sonar <tejas.v.sonar@accenture.com>
2025-11-14 10:46:17 +0530

    path change

--- graphql-schema-diff-poc-full/src/index.ts
+++ graphql-schema-diff-poc-full/src/index.ts
@@ -14,7 +14,7 @@ async function main() {
   if (repoUrl) {
     console.log('Cloning or pulling repo...');
     await cloneOrPull(repoUrl, repoLocalPath);
-    latestSchemaStr = await readSchemaFromRepo(repoLocalPath, process.env.SCHEMA_PATH || './latest-schema.graphql');
+    latestSchemaStr = await readSchemaFromRepo(repoLocalPath, process.env.SCHEMA_PATH || '.schemas/latest-schema.graphql');
   } else {
     latestSchemaStr = await readLocalSchema('./schemas/latest-schema.graphql');
   }
